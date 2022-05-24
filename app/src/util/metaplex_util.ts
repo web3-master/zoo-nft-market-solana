5cc85fbb52a10c63ec3de37a3622429593fea7bf
Daniel Armstrong <dany.armstrong90@gmail.com>
2022-05-24 01:27:43 -0700

    mint feature.
    - dapp init.
    - mint feature.

---
+++ app/src/util/metaplex_util.ts
@@ -0,0 +1,142 @@
+/**
+ * This blob of a file is pulled together from different files from the metaplex
+ * repository.
+ * Metaplex does not have a NPM package at the current time to make this easier, so instead of
+ * trying to reference their stuff, I copied all of the minimum necessary code into this file
+ */
+import { deserializeUnchecked } from "borsh";
+import BN from "bn.js";
+
+export type StringPublicKey = string;
+
+export enum MetadataKey {
+  Uninitialized = 0,
+  MetadataV1 = 4,
+  EditionV1 = 1,
+  MasterEditionV1 = 2,
+  MasterEditionV2 = 6,
+  EditionMarker = 7,
+}
+
+class Creator {
+  address: BN; //StringPublicKey;
+  verified: boolean;
+  share: number;
+
+  constructor(args: {
+    address: BN; //StringPublicKey;
+    verified: boolean;
+    share: number;
+  }) {
+    this.address = args.address;
+    this.verified = args.verified;
+    this.share = args.share;
+  }
+}
+
+class Data {
+  name: string;
+  symbol: string;
+  uri: string;
+  sellerFeeBasisPoints: number;
+  creators: Creator[] | null;
+  constructor(args: {
+    name: string;
+    symbol: string;
+    uri: string;
+    sellerFeeBasisPoints: number;
+    creators: Creator[] | null;
+  }) {
+    this.name = args.name;
+    this.symbol = args.symbol;
+    this.uri = args.uri;
+    this.sellerFeeBasisPoints = args.sellerFeeBasisPoints;
+    this.creators = args.creators;
+  }
+}
+
+class Metadata {
+  key: MetadataKey;
+  updateAuthority: BN; //StringPublicKey;
+  mint: BN; //StringPublicKey;
+  data: Data;
+  primarySaleHappened: boolean;
+  isMutable: boolean;
+  editionNonce: number | null;
+
+  // set lazy
+  masterEdition?: StringPublicKey;
+  edition?: StringPublicKey;
+
+  constructor(args: {
+    updateAuthority: BN; //StringPublicKey;
+    mint: BN; //StringPublicKey;
+    data: Data;
+    primarySaleHappened: boolean;
+    isMutable: boolean;
+    editionNonce: number | null;
+  }) {
+    this.key = MetadataKey.MetadataV1;
+    this.updateAuthority = args.updateAuthority;
+    this.mint = args.mint;
+    this.data = args.data;
+    this.primarySaleHappened = args.primarySaleHappened;
+    this.isMutable = args.isMutable;
+    this.editionNonce = args.editionNonce;
+  }
+}
+
+const METADATA_SCHEMA = new Map<any, any>([
+  [
+    Data,
+    {
+      kind: "struct",
+      fields: [
+        ["name", "string"],
+        ["symbol", "string"],
+        ["uri", "string"],
+        ["sellerFeeBasisPoints", "u16"],
+        ["creators", { kind: "option", type: [Creator] }],
+      ],
+    },
+  ],
+  [
+    Creator,
+    {
+      kind: "struct",
+      fields: [
+        ["address", "u256"],
+        ["verified", "u8"],
+        ["share", "u8"],
+      ],
+    },
+  ],
+  [
+    Metadata,
+    {
+      kind: "struct",
+      fields: [
+        ["key", "u8"],
+        ["updateAuthority", "u256"],
+        ["mint", "u256"],
+        ["data", Data],
+        ["primarySaleHappened", "u8"], // bool
+        ["isMutable", "u8"], // bool
+      ],
+    },
+  ],
+]);
+
+const METADATA_REPLACE = new RegExp("\u0000", "g");
+export const decodeMetadata = (buffer: Buffer): Metadata => {
+  const metadata = deserializeUnchecked(
+    METADATA_SCHEMA,
+    Metadata,
+    buffer
+  ) as Metadata;
+
+  metadata.data.name = metadata.data.name.replace(METADATA_REPLACE, "");
+  metadata.data.uri = metadata.data.uri.replace(METADATA_REPLACE, "");
+  metadata.data.symbol = metadata.data.symbol.replace(METADATA_REPLACE, "");
+  return metadata;
+};
